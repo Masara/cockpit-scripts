895b1cbad554a7fe78dc738174e3476a37417826
Arsam Islami <arsam.islami@kreuzquadrat.com>
2025-03-14 19:17:35 +0100

    Added frontend links to start migration and deletion of v1 files

--- cockpit-cms-Migrate/views/v2/index.php
+++ cockpit-cms-Migrate/views/v2/index.php
@@ -5,10 +5,18 @@
         <kiss-container class="kiss-margin-large">
 
             <ul class="kiss-breadcrumbs">
-                <li><a href="<?=$this->route('/migrate')?>"><?=t('Migrate')?></a></li>
+                <li><a href="<?= $this->route('/migrate') ?>"><?= t('Migrate') ?></a></li>
             </ul>
 
-            <a href="<?=$this->route('/migrate/migrate')?>" target="_blank"><?=t('Migrate (dry-run)')?></a>
+            <p>
+                <a href="<?= $this->route('/migrate/migrate') ?>" target="_blank"><?= t('Migrate (dry-run)') ?></a>
+            </p>
+            <p>
+                <a href="<?= $this->route('/migrate/migrate/all/write') ?>" target="_blank"><?= t('Migrate') ?></a>
+            </p>
+            <p>
+                <a href="<?= $this->route('/migrate/deleteV1stuff') ?>" target="_blank"><?= t('Delete v1 files') ?></a>
+            </p>
 
         </kiss-container>
 
@@ -20,11 +28,9 @@
 
     <script type="module">
 
-        export default {
-        }
+        export default {}
     </script>
 </vue-view>
-
 
 
 <?php //$this->start('app-side-panel') ?>
